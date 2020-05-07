3f79edfc1c35465b29655afe135e4f3de0306482
pp <ppPractice@163.com>
2020-05-07 16:22:40 +0800

    补充了DRY总结

---
+++ 员工手册/开发篇/DRY(不要重复)篇/代码/5代码答案之_在不相干的类中重复/代码/方便削的笔.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 代码
+{
+    class 方便削的笔:铅笔  //这是其中一种解决办法，但是我们今天要展示一种更好的方法,看削笔刀类
+    {
+        public void 削木头笔芯()
+        {
+            削木头();
+            削笔芯();
+        }
+    }
+}
