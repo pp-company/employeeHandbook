1528c2dd16bb3e6033041572a7833178245ef2c1
pp <ppPractice@163.com>
2020-05-11 20:02:41 +0800

    添加了测试框架代码

---
+++ 员工手册/测试篇/框架演示/代码/_画东西.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 代码
+{
+    class _画东西
+    {
+    }
+}
---
+++ 员工手册/测试篇/框架演示/代码/削笔刀测试.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using 代码.test;
+
+namespace 代码
+{
+    class 削笔刀测试:测试模板
+    {
+    }
+}
