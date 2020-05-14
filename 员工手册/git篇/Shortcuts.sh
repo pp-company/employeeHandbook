c14bef8b721d7a8ec12b1e420e8bbca91a135728
John^2 Li <lijohn@usc.edu>
2020-05-14 19:35:46 +0800

    jl edit git shortcut

--- 员工手册/git篇/Shortcuts.sh
+++ 员工手册/git篇/Shortcuts.sh
@@ -28,9 +28,9 @@ git config --global alias.start "!git pl;git checkout master;git nb"
 #9. 删除分支，如果删除不存在的分支会报错，但是没有关系
 git config --global alias.d "branch -d"
 
-#10. 结束任务，之后如果有冲突手动解决冲突，然后马上手动进行push
-git config --global alias.end "!git pl;git checkout master;git m $1"
+#10. 结束任务，end之后如果有冲突手动解决冲突，然后马上手动进行push，（你不push你留着干啥呢？）
+git config --global alias.end "!git pl; git checkout master; git m"
 #11. 更新当前分支
-git config --global alias.rb "! git pl; git rebase master;"
-#解释:当你做了一点点微小的修改尚且不值得merge但是又需要同步一下就用这个，如果你做了很多修改，不建议用这个。按理来说你做的一点点修改不至于导致冲突但是如果冲突发生了用```git add -A; #git rebase --continue;```来解决
+git config --global alias.rb "!git pl; git rebase -Xignore-space-change master;"
+#解释:当你做了一点点微小的修改尚且不值得merge但是又需要同步一下就用这个，如果你做了很多修改，不建议用这个。按理来说你做的一点点修改不至于导致冲突，但是如果冲突发生了，手动解决冲突后用```git add -A; git rebase --continue;```
 #说明:使用git rb应该先把已有的改动cm，如果觉得cm尚早可以用git stash暂存，完成 git rb后（包括解决冲突后） 用git stash pop 放回来
