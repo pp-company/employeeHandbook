50b92ca5a5dc2200673b3d6395a5626ce3237383
pp <ppPractice@163.com>
2020-05-19 17:29:48 +0800

    忽略了一些缓存垃圾

--- 员工手册/测试篇/框架演示/代码/自动铅笔.cs
+++ 员工手册/测试篇/框架演示/代码/自动铅笔.cs
@@ -1,42 +1,42 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using system;
+using system.collections.generic;
+using system.text;
 
-//namespace 代码
-//{
-//    class 自动铅笔:铅笔
-//    {
-//        public 自动铅笔() : base()
-//        {
-           
-//        }
+namespace 代码
+{
+    class 自动铅笔 : 铅笔
+    {
+        public 自动铅笔() : base()
+        {
 
-//        public override int 木头
-//        {
-//            get => base.木头; 
-//            set
-//            {
-//            }
-//        }
-//        public override int 笔芯
-//        {
-//            get => base.笔芯; 
-//            set
-//            {
-//            }
-//        }
+        }
 
-//        public override void 削木头()
-//        {
-//        }
+        public override int 木头
+        {
+            get => base.木头;
+            set
+            {
+            }
+        }
+        public override int 笔芯
+        {
+            get => base.笔芯;
+            set
+            {
+            }
+        }
 
-//        public override void 削笔芯()
-//        {
-//        }
-//        public void 按()
-//        {
-//            _笔芯++;
-//        }
+        public override void 削木头()
+        {
+        }
 
-//    }
-//}
+        public override void 削笔芯()
+        {
+        }
+        public void 按()
+        {
+            _笔芯++;
+        }
+
+    }
+}
