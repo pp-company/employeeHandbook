63c6aa0de6e41fc01e8ca3f0ba1a946a9b3638e7
John^2 Li <lijohn@usc.edu>
2020-05-24 19:25:48 +0800

    comments

--- 员工手册/IO篇/代码/动物代码/2020.05.05（动物）/实现/_使用json读动物园.cs
+++ 员工手册/IO篇/代码/动物代码/2020.05.05（动物）/实现/_使用json读动物园.cs
@@ -5,25 +5,25 @@ using System.Text;
 
 namespace _2020._05._05_动物_
 {
-    class _使用json读动物园: _读动物园, 从一个路径读到一个东西,可以从动物园读取动物的
-    {
-        protected 从一个路径读到一个东西 _从一个路径读到一个东西;
-        protected string _路径;
-        public _使用json读动物园(从一个路径读到一个东西 从一个路径读到一个东西, string 路径,可以显示的 可以显示的) : base(null, 可以显示的)
-        {
-            _从一个路径读到一个东西 = 从一个路径读到一个东西;
-            _路径 = 路径;
-            
-        }
-        public override void 读()
-        {
-            _动物们 = (IEnumerable<会叫的>)读取对象(_路径);
+	class _使用json读动物园 : _读动物园, 从一个路径读到一个东西, 可以从动物园读取动物的
+	{
+		protected 从一个路径读到一个东西 _从一个路径读到一个东西;
+		protected string _路径;
+		public _使用json读动物园(从一个路径读到一个东西 从一个路径读到一个东西, string 路径, 可以显示的 可以显示的) : base(可以显示的)
+		{
+			_从一个路径读到一个东西 = 从一个路径读到一个东西;
+			_路径 = 路径;
 
-        }
+		}
+		public void 读()
+		{
+			_动物们 = (IEnumerable<会叫的>)读取对象(_路径);
 
-        public object 读取对象(string 路径)
-        {
-            return _从一个路径读到一个东西.读取对象(路径);
-        }
-    }
+		}
+
+		public object 读取对象(string 路径)
+		{
+			return _从一个路径读到一个东西.读取对象(路径);
+		}
+	}
 }
--- 员工手册/IO篇/代码/动物代码/2020.05.05（动物）/实现/_写动物园.cs
+++ 员工手册/IO篇/代码/动物代码/2020.05.05（动物）/实现/_写动物园.cs
@@ -10,15 +10,15 @@ using Newtonsoft.Json.Linq;
 
 namespace _2020._05._05_动物_
 {
-    [Serializable]
-    class _写动物园 : _动物园,可以将动物写入到动物园的
-    {
-        public _写动物园(IEnumerable<会叫的> 动物们,可以显示的 可以显示的) : base(动物们,可以显示的)
-        {
-        }
-        public virtual void 写()
-        {
-          
-        }
-    }
+	[Serializable]
+	class _写动物园 : _动物园, 可以将动物写入到动物园的
+	{
+		public _写动物园(IEnumerable<会叫的> 动物们, 可以显示的 可以显示的) : base(动物们, 可以显示的)
+		{
+		}
+		public virtual void 写() //abstract
+		{
+
+		}
+	}
 }
